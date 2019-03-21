d1abe9d905c8e1b6a9b1d8cf2bc793c989022fe5
Junchao Zhong <jczhong115@gmail.com>
2019-03-21 13:16:35 +1300

    - initial commit

---
+++ Quality-Souvenirs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quality_Souvenirs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
