8fa89a4ec23a3ed1b261cf0bc36568d7b9b925f7
Junchao Zhong <jczhong115@gmail.com>
2019-03-22 16:33:57 +1300

    - add more models

---
+++ Quality-Souvenirs/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QualitySouvenirs.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public User User { get; set; }
+        public string OrderStatus { get; set; }
+        public double SubTotal { get; set; }
+        public double GST { get; set; }
+        public double GrandTotal { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Quality-Souvenirs/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QualitySouvenirs.Models
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+        public Order Order { get; set; }
+        public double ItemPrice { get; set; }
+        public Souvenir Souvenir { get; set; }
+        public int Quantity { get; set; }
+    }
+}
