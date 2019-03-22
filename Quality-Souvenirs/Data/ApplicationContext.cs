cf8879d02686099673c9eb7b243adb40d1a6407d
Junchao Zhong <jczhong115@gmail.com>
2019-03-22 16:41:07 +1300

    - initial data set on db context

--- Quality-Souvenirs/Data/ApplicationContext.cs
+++ Quality-Souvenirs/Data/ApplicationContext.cs
@@ -13,11 +13,19 @@ namespace QualitySouvenirs.Data
 
         public DbSet<Souvenir> Souvenirs { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Supplier> suppliers { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderItem> orderItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Souvenir>().ToTable("Souvenir");
             modelBuilder.Entity<Category>().ToTable("Category");
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Supplier>().ToTable("Supplier");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItem");
         }
     }
 }
