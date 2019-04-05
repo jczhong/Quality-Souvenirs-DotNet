13abb1a414d764833cf6b4f8e975e4963b7fb39c
Junchao Zhong <jczhong115@gmail.com>
2019-04-06 00:37:00 +1300

    - add ProductControl and its Index page

---
+++ QualitySouvenirs/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QualitySouvenirs.Data;
+
+namespace QualitySouvenirs.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public ProductController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            return View();
+        }
+    }
+}
