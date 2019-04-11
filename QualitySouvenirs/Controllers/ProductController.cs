3e74cbe06038b89e1f19518de75c1b81163a46c5
Junchao Zhong <jczhong115@gmail.com>
2019-04-11 14:41:24 +1200

    - tune product page layout

--- QualitySouvenirs/Controllers/ProductController.cs
+++ QualitySouvenirs/Controllers/ProductController.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using QualitySouvenirs.Data;
+using QualitySouvenirs.Models;
 
 namespace QualitySouvenirs.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ApplicationContext _context;
+        private readonly List<Category> categories;
 
         public ProductController(ApplicationContext context)
         {
             _context = context;
+            categories = _context.Categories.ToList();
         }
 
         public IActionResult Index(int? id)
         {
+
+            ViewData["Categories"] = categories;
             return View();
         }
     }
