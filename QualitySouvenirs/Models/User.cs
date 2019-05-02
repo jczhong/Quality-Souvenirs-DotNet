dcc47e5ab0d19b3fd21088a656ac99458b0598fd
nini <zjc0507@gmail.com>
2019-05-03 06:37:33 +0800

    registration_2

--- QualitySouvenirs/Models/User.cs
+++ QualitySouvenirs/Models/User.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace QualitySouvenirs.Models
 {
     public class User
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 }
