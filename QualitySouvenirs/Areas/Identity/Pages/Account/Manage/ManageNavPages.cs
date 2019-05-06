ef461db1012931746c798069473ae640b778f3df
Junchao Zhong <jczhong115@gmail.com>
2019-05-06 16:29:11 +1200

    - remove unnecessary function of account management

--- QualitySouvenirs/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ QualitySouvenirs/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -12,29 +12,9 @@ namespace QualitySouvenirs.Areas.Identity.Pages.Account.Manage
 
         public static string ChangePassword => "ChangePassword";
 
-        public static string DownloadPersonalData => "DownloadPersonalData";
-
-        public static string DeletePersonalData => "DeletePersonalData";
-
-        public static string ExternalLogins => "ExternalLogins";
-
-        public static string PersonalData => "PersonalData";
-
-        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
-
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
-
-        public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
-
-        public static string DeletePersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DeletePersonalData);
-
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
-
-        public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
-
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
     
         public static string PageNavClass(ViewContext viewContext, string page)
         {
