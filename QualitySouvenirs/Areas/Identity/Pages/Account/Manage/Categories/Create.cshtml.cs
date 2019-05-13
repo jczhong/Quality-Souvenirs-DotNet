c0ef84525eaffdbd5c99aa102a1ace5f84303a84
Junchao Zhong <jczhong115@gmail.com>
2019-05-13 19:22:25 +1200

    - add Categories management function

---
+++ QualitySouvenirs/Areas/Identity/Pages/Account/Manage/Categories/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using QualitySouvenirs.Data;
+using QualitySouvenirs.Models;
+
+namespace QualitySouvenirs.Areas.Identity.Pages.Account.Manage.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly QualitySouvenirs.Data.ApplicationContext _context;
+
+        public CreateModel(QualitySouvenirs.Data.ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ QualitySouvenirs/Areas/Identity/Pages/Account/Manage/Categories/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QualitySouvenirs.Data;
+using QualitySouvenirs.Models;
+
+namespace QualitySouvenirs.Areas.Identity.Pages.Account.Manage.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly QualitySouvenirs.Data.ApplicationContext _context;
+
+        public EditModel(QualitySouvenirs.Data.ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(Category.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.ID == id);
+        }
+    }
+}
